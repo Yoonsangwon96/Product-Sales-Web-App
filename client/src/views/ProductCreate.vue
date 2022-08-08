7dce2f4cd162c691b6377cc33b201fc1fdab723c
DESKTOP-6759RLR\윤상원 <yoonsangwon0907@naver.com>
2022-08-08 17:23:24 +0900

    Vue cli 세팅, App.vue 파일에 nav html 코드 이식

---
+++ client/src/views/ProductCreate.vue
@@ -0,0 +1,5 @@
+﻿<template>
+    <div class="about">
+        <h1>This is an 상품 등록하기</h1>
+    </div>
+</template>
---
+++ client/src/views/ProductDetail.vue
@@ -0,0 +1,5 @@
+﻿<template>
+    <div class="about">
+        <h1>This is an 상품 상세설명</h1>
+    </div>
+</template>
